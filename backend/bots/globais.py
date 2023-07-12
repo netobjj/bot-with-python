389e405b2b34f4cb2091f639dce772805e1a0dc4
netobjj <123901579+netobjj@users.noreply.github.com>
2023-07-12 16:24:41 -0300

    Update globais.py

--- backend/bots/globais.py
+++ backend/bots/globais.py
@@ -1,5 +1,4 @@
-email = "jose.euclides@alfaconcursos.com.br"
-password = "anleftao12"
+
 
 url_alfa_new_login = "https://www.alfaconcursos.com.br/sessions/new?scrollto=signin"
 url_alfa_new_orgao = "https://www.alfaconcursos.com.br/admin/entity/new"
